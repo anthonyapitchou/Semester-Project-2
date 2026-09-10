0d2ab1185b4929f06daa41bbecbf67db081d416d
Anthony Apitchou <anthony.apicella@stud.noroff.no>
2026-09-10 11:51:16 +0200

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Semester-Project-2/',
   plugins: [
     tailwindcss(),
   ],
